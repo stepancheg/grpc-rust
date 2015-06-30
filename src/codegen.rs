bd53a1c1b246576b11b073e2efa6de2a51cb53ed
Stepan Koltsov <stepan.koltsov@gmail.com>
2015-06-30 04:22:37 +0300

    wip

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,12 +1,32 @@
-extern crate protobuf;
+use std::collections::HashMap;
 
+use protobuf;
 use protobuf::compiler_plugin;
 use protobuf::descriptor::*;
 
 pub fn gen(file_descriptors: &[FileDescriptorProto], files_to_generate: &[String])
         -> Vec<compiler_plugin::GenResult>
 {
-    Vec::new()
+    let files_map: HashMap<&str, &FileDescriptorProto> =
+        file_descriptors.iter().map(|f| (f.get_name(), f)).collect();
+
+    let mut results = Vec::new();
+
+    for file_name in files_to_generate {
+        let file = files_map[&file_name[..]];
+        let base = protobuf::codegen::proto_path_to_rust_base(file.get_name());
+
+        if file.get_service().is_empty() {
+            continue;
+        }
+
+        results.push(compiler_plugin::GenResult {
+            name: base + "_grpc.rs",
+            content: "// XXX\n".to_string().into_bytes(),
+        });
+    }
+
+    results
 }
 
 pub fn protoc_gen_grpc_rust_main() {
