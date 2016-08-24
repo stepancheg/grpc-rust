b7cbc8d35f3be1ed420dacc2b60e990bbae390f5
Stepan Koltsov <stepan.koltsov@gmail.com>
2016-08-24 03:14:48 +0300

    missing files

---
+++ src/misc.rs
@@ -0,0 +1,21 @@
+use std::fmt;
+
+pub struct BsDebug<'a>(pub &'a [u8]);
+
+impl<'a> fmt::Debug for BsDebug<'a> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        try!(write!(fmt, "b\""));
+        let u8a: &[u8] = self.0;
+        for &c in u8a {
+            // ASCII printable
+            if c >= 0x20 && c < 0x7f {
+                try!(write!(fmt, "{}", c as char));
+            } else {
+                try!(write!(fmt, "\\x{:02x}", c));
+            }
+        }
+        try!(write!(fmt, "\""));
+    	Ok(())
+    }
+}
+
