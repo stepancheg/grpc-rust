cdd3ade631ea6077e922f77d0dca65b6726af657
Stepan Koltsov <stepan.koltsov@gmail.com>
2016-11-14 15:30:58 -0800

    Long tests that shows that client may hung because it ignores window

--- long-tests/with-rust/src/bin/long_tests_client.rs
+++ long-tests/with-rust/src/bin/long_tests_client.rs
@@ -20,17 +20,52 @@ use grpc::error::GrpcError;
 
 use std::env;
 
+
+fn single_num_arg_or(cmd_args: &[String], or: u64) -> u64 {
+    if cmd_args.len() == 0 {
+        or
+    } else if cmd_args.len() == 1 {
+        cmd_args[0].parse().expect("failed to parse as u64")
+    } else {
+        panic!("too many args");
+    }
+}
+
+
+fn run_echo(client: LongTestsAsyncClient, cmd_args: &[String]) {
+    let count = single_num_arg_or(cmd_args, 1);
+
+    println!("running {} iterations of echo", count);
+
+    for i in 0..count {
+        let payload = format!("payload {}", i);
+
+        let mut req = EchoRequest::new();
+        req.set_payload(payload.clone());
+
+        let r = client.echo(req).wait().unwrap();
+
+        assert!(payload == r.get_payload());
+    }
+
+    println!("done");
+}
+
+
 fn main() {
+    env_logger::init().unwrap();
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         panic!("too few args")
     }
 
-    let client = LongTestsClient::new("localhost", 23432, false).expect("init");
+    let client = LongTestsAsyncClient::new("localhost", 23432, false).expect("init");
 
     let cmd = &args[1];
+    let cmd_args = &args[2..];
     if cmd == "echo" {
-
+        run_echo(client, cmd_args);
     } else {
         panic!("unknown command: {}", cmd);
     }
