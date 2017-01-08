4bc47fb64301eac24edc367fb0b222c797fdf4e6
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-01-08 21:15:58 +0300

    tool to execute rust/go, go/rust, rust/rust or go/go tests

---
+++ long-tests/run-test-helper.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -e
+
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+me=$0
+
+usage() {
+    die "usage: $me <rust|go> <rust|go>"
+}
+
+cd $(dirname $0)
+
+case $1 in
+    rust) client=../target/debug/long_tests_client ;;
+    go) client=with-go/long_tests_client/long_tests_client ;;
+    *) usage ;;
+esac
+
+case $2 in
+    rust) server=with-go/long_tests_server/long_tests_server ;;
+    go) server=../target/debug/long_tests_server ;;
+    *) usage ;;
+esac
+
+killall -KILL long_tests_server 2> /dev/null || true
+killall -KILL long_tests_client 2> /dev/null || true
+
+$server &
+
+go_server_pid=$!
+
+export RUST_BACKTRACE=1
+
+sleep 0.1 # give server some time to start
+
+$client echo 10000
+
+kill $go_server_pid
