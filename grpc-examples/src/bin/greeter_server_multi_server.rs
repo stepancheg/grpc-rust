968a8c300e21838bd68b9b7ded8e394f4c31e275
siddontang <siddontang@gmail.com>
2017-03-15 10:49:17 +0800

    add multi server example

---
+++ grpc-examples/src/bin/greeter_server_multi_server.rs
@@ -0,0 +1,38 @@
+extern crate futures;
+
+extern crate grpc_examples;
+extern crate grpc;
+
+use std::thread;
+
+use futures::Future;
+
+use grpc::futures_grpc::*;
+use grpc::server::GrpcServerConf;
+
+use grpc_examples::helloworld_grpc::*;
+use grpc_examples::helloworld::*;
+
+struct GreeterImpl;
+
+impl GreeterAsync for GreeterImpl {
+    fn SayHello(&self, req: HelloRequest) -> GrpcFutureSend<HelloReply> {
+        let mut r = HelloReply::new();
+        let name = if req.get_name().is_empty() { "world" } else { req.get_name() };
+        println!("greeting request from {}", name);
+        r.set_message(format!("Hello {}", name));
+        Box::new(futures::finished(r))
+    }
+}
+
+fn main() {
+    let mut conf = GrpcServerConf::default();
+    conf.http.reuse_port = Some(true);
+
+    let _server1 = GreeterAsyncServer::new("[::]:50051", conf.clone(), GreeterImpl);
+    let _server2 = GreeterAsyncServer::new("[::]:50051", conf, GreeterImpl);
+
+    loop {
+        thread::park();
+    }
+}
