edd45f3fc6cbe989d82b93bea39accd04c27d189
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-05-06 00:30:34 +0300

    ver++

--- update-version.sh
+++ update-version.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
-sed -e 's,^version = .*,version = "0.1.2",' -i '' \
+version="0.1.3"
+
+sed -e 's,^version = .*,version = "'$version'",' -i '' \
     Cargo.toml http2/Cargo.toml grpc-compiler/Cargo.toml
 
-sed -e '/httpbis.*path/ s,version = [^ ]*,version = "0.1.2",' -i '' Cargo.toml
+sed -e '/httpbis.*path/ s,version = [^ ]*,version = "'$version'",' -i '' Cargo.toml
 
 # vim: set ts=4 sw=4 et:
