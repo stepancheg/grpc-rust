3e5ecde9a926c331bd2bd8efdb5de61bcb5c6b40
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-06-24 21:29:20 +0300

    Should not update httpbis version

--- update-version.sh
+++ update-version.sh
@@ -5,6 +5,4 @@ version="0.1.10"
 sed -e 's,^version = .*,version = "'$version'",' -i '' \
     Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
 
-sed -e '/httpbis.*path/ s,version = [^ ]*,version = "'$version'",' -i '' Cargo.toml
-
 # vim: set ts=4 sw=4 et:
