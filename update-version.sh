b64fe0d5894dc1a50dee913f6537f04d5384cf04
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-06-24 21:34:45 +0300

    v0.1.11

--- update-version.sh
+++ update-version.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -e
 
-version="0.1.10"
+version="0.1.11"
 
 sed -e 's,^version = .*,version = "'$version'",' -i '' \
     Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
+
+sed -e '/grpc/ s,^version = .*,version = "'$version'",' -i '' \
+    protoc-rust-grpc/Cargo.toml
 
 # vim: set ts=4 sw=4 et:
