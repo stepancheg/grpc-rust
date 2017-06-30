356b93a41e7cd96058c0f5c2b8c7549b3e865921
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-06-30 04:53:54 +0300

    Print instance number in greeter_server_multi_server

--- grpc-examples/src/bin/greeter_server_multi_server.rs
+++ grpc-examples/src/bin/greeter_server_multi_server.rs
@@ -9,13 +9,15 @@ use std::thread;
 use grpc_examples::helloworld_grpc::*;
 use grpc_examples::helloworld::*;
 
-struct GreeterImpl;
+struct GreeterImpl {
+    instance: u32,
+}
 
 impl Greeter for GreeterImpl {
     fn say_hello(&self, _m: grpc::RequestOptions, req: HelloRequest) -> grpc::SingleResponse<HelloReply> {
         let mut r = HelloReply::new();
         let name = if req.get_name().is_empty() { "world" } else { req.get_name() };
-        println!("greeting request from {}", name);
+        println!("instance {}, greeting request from {}", self.instance, name);
         r.set_message(format!("Hello {}", name));
         grpc::SingleResponse::completed(r)
     }
@@ -29,13 +31,13 @@ fn main() {
     let mut server1 = grpc::ServerBuilder::new_plain();
     server1.http.conf = conf.clone();
     server1.http.set_port(50051);
-    server1.set_service(GreeterServer::new_service_def(GreeterImpl));
+    server1.set_service(GreeterServer::new_service_def(GreeterImpl { instance: 1 }));
     let _server1 = server1.build().expect("server 1");
 
     let mut server2 = grpc::ServerBuilder::new_plain();
     server2.http.conf = conf.clone();
     server2.http.set_port(50051);
-    server2.set_service(GreeterServer::new_service_def(GreeterImpl));
+    server2.set_service(GreeterServer::new_service_def(GreeterImpl { instance: 2 }));
     let _server2 = server2.build().expect("server 1");
 
     loop {
