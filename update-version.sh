b2e9d5dbca041560fbf7ad393a827e2947368e5a
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-07-06 13:46:01 +0300

    Fix update version and fix build

--- update-version.sh
+++ update-version.sh
@@ -5,7 +5,7 @@ version="0.2.0"
 sed -e 's,^version = .*,version = "'$version'",' -i '' \
     Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
 
-sed -e '/grpc/ s,^version = .*,version = "'$version'",' -i '' \
+sed -e '/grpc/ s,version = "[^"]*",version = "'$version'",' -i '' \
     protoc-rust-grpc/Cargo.toml
 
 # vim: set ts=4 sw=4 et:
