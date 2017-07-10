5c6bfa1e48166ef3376e8745bafc4874ce3d8886
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-07-10 22:41:17 +0300

    Update version to fix broken deps in 0.2.0

--- update-version.sh
+++ update-version.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
-version="0.2.0"
+version="0.2.1"
 
 sed -e 's,^version = .*,version = "'$version'",' -i '' \
-    Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
+    grpc/Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
 
 sed -e '/grpc/ s,version = "[^"]*",version = "'$version'",' -i '' \
     protoc-rust-grpc/Cargo.toml
