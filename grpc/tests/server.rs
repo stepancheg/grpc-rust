544aafa1078ec5bccf51e2dbb5553736771ede8d
Bryan Blanchard <bryan.blanchard@gmail.com>
2017-08-02 07:46:58 -0400

    Support Unix domain sockets
    Add options to client and serverbuilder for generating servers/clients
    which use Unix domain sockets.
    
    Addresses #91

--- grpc/tests/server.rs
+++ grpc/tests/server.rs
@@ -43,7 +43,7 @@ fn multiple_services() {
 
     let server = server.build().expect("server");
 
-    let client = Client::new_plain("::1", server.local_addr().port(), ClientConf::new())
+    let client = Client::new_plain("::1", server.local_addr().port().unwrap(), ClientConf::new())
         .expect("client");
 
     assert_eq!(
@@ -55,4 +55,43 @@ fn multiple_services() {
         "zyx".to_owned(),
         client.call_unary(
             RequestOptions::new(), "xyz".to_owned(), reverse).wait_drop_metadata().unwrap());
+}
+
+#[cfg(unix)]
+#[test]
+fn single_service_unix() {
+    let test_socket_address = "/tmp/grpc_rust_single_service_unix";
+    let mut server = ServerBuilder::new_plain();
+    server.http.set_unix_addr(test_socket_address.to_owned()).unwrap();
+
+    let echo = string_string_method("/foo/echo", GrpcStreaming::Unary);
+    let reverse = string_string_method("/bar/reverse", GrpcStreaming::Unary);
+
+    server.add_service(ServerServiceDefinition::new("/foo", vec![
+        ServerMethod::new(
+            echo.clone(),
+            MethodHandlerUnary::new(echo_fn))
+    ]));
+
+    server.add_service(ServerServiceDefinition::new("/bar", vec![
+        ServerMethod::new(
+            reverse.clone(),
+            MethodHandlerUnary::new(reverse_fn))
+    ]));
+
+    let _server = server.build().expect("server");
+
+    let client = Client::new_plain_unix(test_socket_address, ClientConf::new())
+        .expect("client");
+
+    assert_eq!(
+        "abc".to_owned(),
+        client.call_unary(
+            RequestOptions::new(), "abc".to_owned(), echo).wait_drop_metadata().unwrap());
+
+    assert_eq!(
+        "zyx".to_owned(),
+        client.call_unary(
+            RequestOptions::new(), "xyz".to_owned(), reverse).wait_drop_metadata().unwrap());
+
 }
