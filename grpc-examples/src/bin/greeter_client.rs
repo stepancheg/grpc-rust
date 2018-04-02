2c80cbbaffac29b0d8fb9c649951b41f916e42e6
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-04-02 13:32:28 +0100

    Connect to ::1 instead of localhost

--- grpc-examples/src/bin/greeter_client.rs
+++ grpc-examples/src/bin/greeter_client.rs
@@ -11,7 +11,7 @@ use std::env;
 fn main() {
     let name = env::args().nth(1).map(|s| s.to_owned()).unwrap_or_else(|| "world".to_owned());
 
-    let client = GreeterClient::new_plain("localhost", 50051, Default::default()).unwrap();
+    let client = GreeterClient::new_plain("::1", 50051, Default::default()).unwrap();
 
     let mut req = HelloRequest::new();
     req.set_name(name);
--- grpc-examples/src/bin/greeter_client_multi.rs
+++ grpc-examples/src/bin/greeter_client_multi.rs
@@ -13,7 +13,7 @@ use std::env;
 fn main() {
     let name = env::args().nth(1).map(|s| s.to_owned()).unwrap_or_else(|| "world".to_owned());
 
-    let client = Client::new_plain("localhost", 50051, Default::default()).unwrap();
+    let client = Client::new_plain("::1", 50051, Default::default()).unwrap();
     let greeter_client = GreeterClient::with_client(client.clone());
     let greeter_client2 = GreeterClient::with_client(client);
 
