d0b516c948fdc70524ae653747690ae63dba2e4c
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-04-11 17:53:49 +0100

    Use exact dependency versions internally

--- update-version.sh
+++ update-version.sh
@@ -5,7 +5,7 @@ version="0.2.1"
 sed -e 's,^version = .*,version = "'$version'",' -i '' \
     grpc/Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
 
-sed -e '/grpc/ s,version = "[^"]*",version = "'$version'",' -i '' \
+sed -e '/grpc/ s,version = "[^"]*",version = "='$version'",' -i '' \
     protoc-rust-grpc/Cargo.toml
 
 # vim: set ts=4 sw=4 et:
