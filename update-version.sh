3ff10cc2906f4e2b59718eb3cf21609f2ba69a07
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-04-11 17:54:30 +0100

    Bump version to 0.3.0

--- update-version.sh
+++ update-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-version="0.2.1"
+version="0.3.0"
 
 sed -e 's,^version = .*,version = "'$version'",' -i '' \
     grpc/Cargo.toml grpc-compiler/Cargo.toml protoc-rust-grpc/Cargo.toml
