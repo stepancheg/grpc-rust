e78fbfade3f613b0a635642382ebff1978e6c1d6
Stepan Koltsov <stepan.koltsov@gmail.com>
2020-01-12 00:59:36 +0000

    Better move reliable tests

--- ci/run.sh
+++ ci/run.sh
@@ -8,12 +8,16 @@ export RUST_BACKTRACE=1
 
 ci/install-protobuf.sh
 
-cargo install protobuf-codegen --version '2'
+cargo test --all --all-targets
 
-cargo test --all
+(
+    cargo install protobuf-codegen --version '2'
 
-./grpc-compiler/test-protoc-plugin/gen.sh
+    cd grpc-compiler/test-protoc-plugin
 
-cargo check --all
+    ./gen.sh
+
+    cargo check --all
+)
 
 # vim: set ts=4 sw=4 et:
