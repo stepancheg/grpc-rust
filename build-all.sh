cae5e02c5a1e36325904486a8b6f16270995423d
Stiopa Koltsov <nga@fb.com>
2020-02-10 23:53:37 +0000

    Unused

--- build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-cargo build
-cargo build --manifest-path=grpc-compiler/Cargo.toml
-cargo build --manifest-path=grpc-examples/Cargo.toml
-cargo build --manifest-path=long-tests/with-rust/Cargo.toml
-cargo build --manifest-path=interop/Cargo.toml
-
-# vim: set ts=4 sw=4 et:
