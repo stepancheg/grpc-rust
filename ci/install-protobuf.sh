cd1018e136131cca261f8773f9e4914edcaad5eb
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-03-29 00:27:56 +0100

    Happier shell lint

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -11,21 +11,21 @@ test -n "$PROTOBUF_VERSION" || die "PROTOBUF_VERSION env var is undefined"
 
 case "$PROTOBUF_VERSION" in
 2*)
-    basename=protobuf-$PROTOBUF_VERSION
+    basename="protobuf-$PROTOBUF_VERSION"
     ;;
 3*)
-    basename=protobuf-cpp-$PROTOBUF_VERSION
+    basename="protobuf-cpp-$PROTOBUF_VERSION"
     ;;
 *)
     die "unknown protobuf version: $PROTOBUF_VERSION"
     ;;
 esac
 
-curl -sL https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
+curl -sL "https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz" | tar zx
 
-cd protobuf-$PROTOBUF_VERSION
+cd "protobuf-$PROTOBUF_VERSION"
 
-./configure --prefix=$HOME && make -j2 && make install
+./configure --prefix="$HOME" && make -j2 && make install
 
 test -n "$GITHUB_PATH"
-echo "$HOME/bin" >>$GITHUB_PATH
+echo "$HOME/bin" >>"$GITHUB_PATH"
